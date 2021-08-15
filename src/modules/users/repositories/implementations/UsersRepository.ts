df41a30cdbd828f69995a59d26dfc16a6d73c56a
João Lucas <lucas_0423@hotmail.com>
2021-08-15 13:22:30 -0300

    Resolvendo desafio 01 chapter 03

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,23 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    const user = await this.repository.findOne(user_id, {
+      relations: ['games']
+    })
+
+    return user!;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query('SELECT * FROM Users ORDER BY first_name');
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`SELECT * FROM users 
+    WHERE LOWER(first_name) = '${first_name.toLowerCase()}'
+    AND LOWER(last_name) = '${last_name.toLowerCase()}'`);
   }
 }
